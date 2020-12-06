f43ff8b5c3e0ef258911cb5911111ee9f17c2a19
John Diego <johnoliveirasilva9@gmail.com>
2020-12-06 17:44:09 -0300

    Strings Basic Methods

--- Basic_Ruby/basic-data-types/Strings.rb
+++ Basic_Ruby/basic-data-types/Strings.rb
@@ -25,3 +25,50 @@ name = "Odin"
 
 puts "Hello, #{name}" #=> "Hello, Odin"
 puts 'Hello, #{name}' #=> "Hello, #{name}"
+
+# Common String Methods
+
+# capitalize
+"hello".capitalize #=> "Hello"
+
+#include?
+"hello".include?("lo") #=> true
+"hello".include?("z") #=> false
+
+#upcase
+"hello".upcase #=> "HELLO"
+
+#downcase
+"Hello".downcase #=> "hello"
+
+#empty?
+"hello".empty?  #=> false
+"".empty?       #=> true
+
+#reverse
+"hello".reverse  #=> "olleh"
+
+#split
+"hello world".split  #=> ["hello", "world"]
+"hello".split("")    #=> ["h", "e", "l", "l", "o"]
+
+#strip ( removing spaces)
+" hello, world   ".strip  #=> "hello, world"
+
+"he77o".sub("7", "l")           #=> "hel7o"
+
+"he77o".gsub("7", "l")          #=> "hello"
+
+"hello".insert(-1, " dude")     #=> "hello dude"
+
+"hello world".delete("l")       #=> "heo word"
+
+"!".prepend("hello, ", "world") #=> "hello, world!"
+
+#Converting other objects to Strings
+
+5.to_s        #=> "5"
+
+nil.to_s      #=> ""
+
+:symbol.to_s  #=> "symbol"
