a606f940190901a104c407ec1d9dd9edcf71eb4c
John Diego <johnoliveirasilva9@gmail.com>
2020-12-06 16:13:13 -0300

    Concatenationn

---
+++ Basic_Ruby/basic-data-types/Strings.rb
@@ -0,0 +1,11 @@
+# Concatenation
+
+# With the plus operator:
+"Welcome " + "to " + "Odin!" #=> "Welcome to Odin!"
+
+# With the shovel operator:
+
+"Welcome " << "to " << "Odin!" #=> "Welcome to Odin!"
+
+# With the concat method:
+"Welcome ".concat("to ").concat("Odin!") #=> "Welcome to Odin!"
