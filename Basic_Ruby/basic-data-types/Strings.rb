e202f48e0ac67ec6758b1ac8df82ed745fc65a0c
John Diego <johnoliveirasilva9@gmail.com>
2020-12-06 16:28:21 -0300

    Substrings

--- Basic_Ruby/basic-data-types/Strings.rb
+++ Basic_Ruby/basic-data-types/Strings.rb
@@ -9,3 +9,13 @@
 
 # With the concat method:
 "Welcome ".concat("to ").concat("Odin!") #=> "Welcome to Odin!"
+
+# Substrings
+
+"hello"[0]      #=> "h"
+
+"hello"[0..1]   #=> "he"
+
+"hello"[0, 4]   #=> "hell"
+
+"hello"[-1]     #=> "o"
