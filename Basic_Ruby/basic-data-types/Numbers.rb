19ff7975cc5bfa05c8044dfcf309aa244eb3c73a
John Diego <johnoliveirasilva9@gmail.com>
2020-12-06 16:02:35 -0300

    Converting Number Types

--- Basic_Ruby/basic-data-types/Numbers.rb
+++ Basic_Ruby/basic-data-types/Numbers.rb
@@ -23,3 +23,12 @@
 
 17 / 5 #=> 3, not 3.4
 17 / 5.0 #=> 3.4
+
+# Converting Number Types
+
+# To convert an integer to a float:
+13.to_f  #=> 13.0
+
+# To convert a float to an integer:
+13.0.to_i #= 13
+13.9.to_i #= 13
