9cb8360074ffd587dba32ca1fc8b0d285caaa933
John Diego <johnoliveirasilva9@gmail.com>
2020-12-07 20:38:11 -0300

    Input commands gets

--- Basic_Ruby/input_and_output/output.rb
+++ Basic_Ruby/input_and_output/output.rb
@@ -2,3 +2,12 @@
 print "Learding to code is FUN!"
 
 puts "Learding to code is FUN!" #=> New line
+
+
+# input commands
+gets
+The Odin Project
+=> "The Odin Project"
+
+new_string = gets.chomp #=> chomp remove new-line
+puts new_string
